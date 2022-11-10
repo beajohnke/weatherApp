568b827cd030bcd65916f93e34b57e9b81261288
Beatriz Johnke <104087110+beajohnke@users.noreply.github.com>
2022-11-10 10:40:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ const showWeatherData = async (city) => {
 	
 	if (data.cod === "404") {
 		showErrorMessage();
-		containerEl.style.height = '200px';
+		containerEl.style.height = '250px';
 		weatherDataContainer.classList.add("hide");
 		return;
 	}
